68f014555c2bc974b6c847c4bcc67f0a70d5a6f3
Vasuki <vasukiabi@gmail.com>
2020-06-20 22:31:38 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'onewaybinding';
+  selectedDay: string = '';
+
+
+  //event handler for the select element's change event
+  selectChangeHandler (event: any) {
+    //update the ui
+    this.selectedDay = event.target.value;
+   // console.log(this.selectedDay);
+    console.log(event.target.value);
+    if(this.selectedDay=="camel")
+    {
+      this.selectedDay="./assets/image/camel.jpg";
+    }else if(this.selectedDay=="Tiger")
+    {
+      this.selectedDay="./assets/image/tiger.jpg";
+    }
+    else if(this.selectedDay=="elephant")
+    {
+      this.selectedDay="./assets/image/elephant.jpg";
+    }
+}
 }
